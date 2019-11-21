ba65314bfa967f0c695932a361a0671d9c9e6b3a
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2019-11-20 22:04:16 -0300

    Update PedidoDeFinalizacao.java
    Resolvido javadocs ; )

--- Projeto/Cliente/PedidoDeFinalizacao.java
+++ Projeto/Cliente/PedidoDeFinalizacao.java
@@ -1,2 +1,7 @@
+/**
+	A classe pedidoDeFinalizacao representa um pedido de finalizacao de determinado Cliente.
+	@author Eduardo Migueis, Enzo Spinella, Gabriel Scalese e Nícolas Duarte
+	@since 2019
+*/
 public class PedidoDeFinalizacao extends Comunicado
 {}
