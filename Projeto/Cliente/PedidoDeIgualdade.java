5575dfa76d6832c735b632a4a05f887ebd1ae40b
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2019-11-20 22:02:26 -0300

    Update PedidoDeIgualdade.java

--- Projeto/Cliente/PedidoDeIgualdade.java
+++ Projeto/Cliente/PedidoDeIgualdade.java
@@ -1,2 +1,7 @@
+/**
+	A classe pedidoDeIgualdade representa um pedido de igualdade de determinado Cliente.
+	@author Eduardo Migueis, Enzo Spinella, Gabriel Scalese e Nícolas Duarte
+	@since 2019
+*/
 public class PedidoDeIgualdade extends Comunicado
 {}
