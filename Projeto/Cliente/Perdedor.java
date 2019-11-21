b479dfb94bc4390f056271c1e18be830bba0a3d6
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2019-11-20 22:12:38 -0300

    Update Perdedor.java

--- Projeto/Cliente/Perdedor.java
+++ Projeto/Cliente/Perdedor.java
@@ -1,7 +1,18 @@
+/**
+	A classe Perdedor representa um perdedor,
+	tendo como base uma String que respresenta o nome do mesmo.
+	Instâncias desta classe permitem armazenar determinado perdedor.
+	@author Eduardo Migueis, Enzo Spinella, Gabriel Scalese e Nícolas Duarte
+	@since 2019
+*/
 public class Perdedor extends Comunicado
 {
 	private String perdedor;
 
+/**
+	Constroi uma instância da classe Perdedor.
+	@param nome determina o perdedor a ser armazenado na classe.
+*/
 	public Perdedor(String perdedor) throws Exception
 	{
 		if(perdedor == null)
@@ -9,7 +20,16 @@ public class Perdedor extends Comunicado
 
 		this.perdedor = perdedor;
 	}
-
+	
+	/**
+	    Constroi uma cópia da instância da classe Perdedor.
+	    Para tanto, deve ser fornecida uma instancia da classe
+	    Perdedor para ser utilizada como modelo para a
+	    construção da nova instância.
+	    @param perd a instância da classe Perdedor a ser usada
+            como modelo.
+	    @throws Exception se o modelo for null.
+*/
 	public Perdedor(Perdedor perd) throws Exception
 		{
 			if(perd == null)
@@ -17,12 +37,21 @@ public class Perdedor extends Comunicado
 
 			this.perdedor = perd.perdedor;
 	}
-
+/**
+	Retorna o perdedor armazenado.
+	@return retorna o perdedor armazenado.
+*/
 	public String getPerdedor()
 	{
 		return this.perdedor;
 	}
-
+/**
+	    Clona um Perdedor.
+	    Produz e resulta uma cópia do Perdedor representado pela instância
+	    à qual o método for aplicado.
+	    @return a cópia do Perdedor representada pela instância à qual
+	    o método for aplicado.
+*/
 	public Object clone()
 	{
 		Perdedor ret = null;
@@ -36,12 +65,26 @@ public class Perdedor extends Comunicado
 
 		return ret;
 	}
-
+/**
+	    Gera uma representação textual do perdedor.
+	    Produz e resulta um String com o perdedor.
+	    @return um String contendo o conteúdo do perdedor.
+	    */
 	public String toString()
 	{
 		return "Perdedor: " + this.perdedor;
 	}
-
+/**
+	    Verifica a igualdade entre dois Perdedores.
+	    Verifica se o Object fornecido como parâmetro representa um
+	    Perdedor igual àquela representada pela instância à qual este
+	    método for aplicado, resultando true em caso afirmativo,
+	    ou false, caso contrário.
+	    @param  obj o objeto a ser comparado com a instância à qual esse método
+	            for aplicado.
+	    @return true, caso o Object fornecido ao método e a instância chamante do
+	            método representarem agendas iguais, ou false, caso contrário.
+	    */
 	public boolean equals(Object obj)
 	{
 		if(obj == null)
@@ -59,7 +102,12 @@ public class Perdedor extends Comunicado
 
 		return false;
 	}
-
+/**
+	 Calcula o código de espalhamento (ou código de hash) de um Perdedor.
+	 Calcula e resulta o código de espalhamento (ou código de hash, ou ainda o
+	 hashcode) do Perdedor representada pela instância à qual o método for aplicado.
+	 @return o código de espalhamento do Perdedor chamante do método.
+*/
 	public int hashCode()
 	{
 		int ret = 17;
