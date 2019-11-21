be160217e6e60879aadc5b9eb883913612b4daca
Nícolas Duarte <49459951+GreenLinho@users.noreply.github.com>
2019-11-21 06:58:13 -0300

    Correções

--- Projeto/Cliente/PedidoDePalavra.java
+++ Projeto/Cliente/PedidoDePalavra.java
@@ -1,7 +1,7 @@
 /**
-	A classe pedido de palavra representa um pedido de palavra de determinado Cliente.
-	@author Eduardo Migueis, Enzo Spinella, Gabriel Scalese e Nícolas Duarte
-	@since 2019
+	A classe PedidoDePalavra representa um pedido de palavra de determinado Cliente feito ao servidor.
+	@author Eduardo Migueis, Enzo Spinella, Gabriel Scalese e Nicolas Duarte.
+	@since 2019.
 */
 public class PedidoDePalavra extends Comunicado
 {}
