7f07845c5590627e7ab891baf1a1aaa37dfb61af
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2019-11-20 22:14:52 -0300

    Update PedidoDePalavra.java

--- Projeto/Cliente/PedidoDePalavra.java
+++ Projeto/Cliente/PedidoDePalavra.java
@@ -1,2 +1,7 @@
+/**
+	A classe pedido de palavra representa um pedido de palavra de determinado Cliente.
+	@author Eduardo Migueis, Enzo Spinella, Gabriel Scalese e Nícolas Duarte
+	@since 2019
+*/
 public class PedidoDePalavra extends Comunicado
 {}
