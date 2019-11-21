663e0e29a56e7e453c740ef4c0d223d17f4a201b
Nícolas Duarte <49459951+GreenLinho@users.noreply.github.com>
2019-11-21 06:58:48 -0300

    Correções

--- Projeto/Cliente/PedidoDeIgualdade.java
+++ Projeto/Cliente/PedidoDeIgualdade.java
@@ -1,7 +1,7 @@
 /**
 	A classe PedidoDeIgualdade representa um pedido ao servidor, que diz que a palavra digitada pelo usuario e valida.
-	@author Eduardo Migueis, Enzo Spinella, Gabriel Scalese e Nicolas Duarte
-	@since 2019
+	@author Eduardo Migueis, Enzo Spinella, Gabriel Scalese e Nicolas Duarte.
+	@since 2019.
 */
 public class PedidoDeIgualdade extends Comunicado
 {}
