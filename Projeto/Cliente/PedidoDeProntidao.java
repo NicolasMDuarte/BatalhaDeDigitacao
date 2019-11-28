c528007088e8d2b59937e201d3757b12522b85e8
Nícolas Duarte <49459951+GreenLinho@users.noreply.github.com>
2019-11-28 09:23:06 -0300

    javadocs

--- Projeto/Cliente/PedidoDeProntidao.java
+++ Projeto/Cliente/PedidoDeProntidao.java
@@ -1,2 +1,7 @@
+/**
+  A classe PedidoDeProntidao diz ao servidor que o jogador já está pronto para a próxima partida.
+	@author Eduardo Migueis, Enzo Spinella, Gabriel Scalese e Nicolas Duarte.
+	@since 2019.
+*/
 public class PedidoDeProntidao extends Comunicado
 {}
