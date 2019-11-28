6334f12bf031dc3da9ad6a1d1854eb9f2feee7cb
Nícolas Duarte <49459951+GreenLinho@users.noreply.github.com>
2019-11-28 09:27:23 -0300

    javadocs

--- Projeto/Servidor/PedidoDeQuantidadeDeUsuarios.java
+++ Projeto/Servidor/PedidoDeQuantidadeDeUsuarios.java
@@ -1,2 +1,7 @@
+/**
+  A classe PedidoDeQuantidadeDeUsuarios pede ao servidor para que ele mande a quantidade de usuarios conectados.
+	@author Eduardo Migueis, Enzo Spinella, Gabriel Scalese e Nicolas Duarte.
+	@since 2019.
+*/
 public class PedidoDeQuantidadeDeUsuarios extends Comunicado
 {}
